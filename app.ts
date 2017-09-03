5e30edb5b60c828942c2ed2c1294554f24882ac7
nitslaszlo <nitslaszlo@gmail.com>
2017-09-03 23:42:00 +0200

    Upload

--- app.ts
+++ app.ts
@@ -8,7 +8,12 @@
 
         Content(req: any, res: any): void {
             res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+            res.write("<html><head><title>MyApp</title></head>");
+            res.write("<body><pre style='font-family: Courier';>");
+
             res.write("<h1>Hello World!</h1>");
+
+            res.write("</pre></body>");
             res.end();
         }
     }
