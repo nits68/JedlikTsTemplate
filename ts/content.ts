447b0713ddbf9478731c18183a622b45c422b9bc
nitslaszlo <nitslaszlo@gmail.com>
2019-01-30 18:54:21 +0100

    up

--- ts/content.ts
+++ ts/content.ts
@@ -1,4 +1,5 @@
 ﻿import * as http from "http";
+import { read } from "fs";
 // import * as url from "url"; // űrlapokhoz, input kiolvasás
 // import * as fs from "fs"; // file-kezelés
 
@@ -6,9 +7,9 @@ export default class Content {
 
     Content(req: any, res: http.ServerResponse): void {
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-        res.write("<p>Hello Node.js!</p>");
-        res.write("<p>Hello TypeScript!</p>");
-        res.write("<p>Hello Heroku!</p>");
+        res.write("<p style='color: red;'>Hello Node.js!</p>");
+        res.write("<p style='color: green;'>Hello TypeScript!</p>");
+        res.write("<p style='color: blue;'>Hello Heroku!</p>");
 
         res.end();
     }
