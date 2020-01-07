e9f6d21266929b8c54c94d7aaf7b43b768dd36e8
nitslaszlo <nitslaszlo@gmail.com>
2020-01-07 09:38:30 +0100

    up

--- js/content.js
+++ js/content.js
@@ -10,10 +10,16 @@ class Content {
             return;
         }
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+        res.write("<!DOCTYPE html>");
+        res.write("<html lang='hu'>");
+        res.write("<head><title>JedlikTsTemplate</title></head>");
+        res.write("<body><pre style='font-size:18px;font-weight:bold;'>");
         res.write("<h1 style='color: red;'>Hello Node.js</h1>");
         res.write("<h1 style='color: green;'>Hello TypeScript!</h1>");
         res.write("<h1 style='color: blue;'>Hello Heroku!</h1>");
         res.write("<h1 style='color: black;'>Hello Jedlik!!!!!</h1>");
+        const sorszám = 1;
+        res.write(`${sorszám}. feladat`);
         res.write("<b>Fejlesztői környezet telepítésének leírása, forráskód GitHub repository:</b><br>");
         res.write("<a href='https://github.com/nitslaszlo/JedlikTsTemplate' target='_blank'>https://github.com/nitslaszlo/JedlikTsTemplate</a><br>");
         res.end();
--- ts/content.ts
+++ ts/content.ts
@@ -9,12 +9,18 @@ export default class Content {
             fs.createReadStream("favicon.ico").pipe(res);
             return;
         }
-
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+        res.write("<!DOCTYPE html>");
+        res.write("<html lang='hu'>");
+        res.write("<head><title>JedlikTsTemplate</title></head>");
+        res.write("<body><pre style='font-size:18px;font-weight:bold;'>");
+
         res.write("<h1 style='color: red;'>Hello Node.js</h1>");
         res.write("<h1 style='color: green;'>Hello TypeScript!</h1>");
         res.write("<h1 style='color: blue;'>Hello Heroku!</h1>");
-        res.write("<h1 style='color: black;'>Hello Jedlik!!!!!</h1>");
+        res.write("<h1 style='color: black;'>Hello Jedlik!</h1>");
+        const sorszám = 1;
+        res.write(`${sorszám}. feladat`);
 
         res.write("<b>Fejlesztői környezet telepítésének leírása, forráskód GitHub repository:</b><br>");
         res.write("<a href='https://github.com/nitslaszlo/JedlikTsTemplate' target='_blank'>https://github.com/nitslaszlo/JedlikTsTemplate</a><br>");
