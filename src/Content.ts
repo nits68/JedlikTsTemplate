13ae571e5f315d685b104178cc4a8c4725ab281c
László Nits <nitslaszlo@gmail.com>
2020-03-27 00:21:54 +0100

    up

--- src/Content.ts
+++ src/Content.ts
@@ -2,11 +2,6 @@
 import http from "http";
 import url from "url";
 
-interface InputInterface {
-    name: string;
-    age: number;
-    male: boolean;
-}
 export default class Content {
     public content(req: http.IncomingMessage, res: http.ServerResponse): void {
         // favicon.ico kérés kiszolgálása:
@@ -25,6 +20,9 @@ export default class Content {
         res.write("<title>Jedlik Ts Template</title>");
         res.write("</head>");
         res.write("<body><form><pre>");
+        // URL paraméter(ek) elérése:
+        const params = url.parse(req.url as string, true).query;
+        // Ha nincsenek paraméterek, akkor tedd megjegyzésbe a fenti sort!
 
         // Kezd a kódolást innen -->
 
@@ -33,27 +31,13 @@ export default class Content {
         // Tetszőleges html teg-ek és attribútumok beépítése:
         res.write("<span style='color: blue;'><i>Színes és dőlt Hello World!'</i></span>\n");
 
-        // Egyszerű input JSON állományból:
-        const input: InputInterface = JSON.parse(fs.readFileSync("input.json", "utf-8"));
+        // Próbáljuk számra konvertálni a "kor" paraméter (http://localhost:8080/?kor=16) értékét:
+        const korod: number = parseInt(params.kor as string);
+        // Ha nincs "kor" paraméter megadva, vagy nem lehet számra konvertálni értékét,
+        // akkor a "korod" változóba NaN érték kerül
 
-        // String template használata
-        res.write(`1. feladat: ${input.name} kora: ${input.age}\n`);
-
-        // Változó definiálása:
-        let neme = "";
-        neme = input.male ? "férfi" : "nő";
-        res.write(`2. feladat: ${input.name} neme: ${neme}\n`);
-
-        // Input form-al és <input type='number'>-el:
-        // URL paraméter(ek) (itt "kor") ellenőrzése,  kiolvasása, alapértelmezett értéke 18:
-        const u = url.parse(req.url as string, true).query;
-        // ha "kor" paraméter nincs megadva vagy "kor" paraméter üres string,
-        // akkor legyen 18 az értéke, egyébként konvertáljuk számra a "kor" paraméter értékét:
-        // let kor: number = u.kor === undefined || u.kor === "" ? 18 : parseInt(u.kor as string);
-        let kor: number = parseInt(u.kor as string);
-        if (isNaN(kor) || kor < 0 || kor > 99) kor = 18; // egy kis ellenőrzés
-        res.write(`3. feladat: Kérem a korod [0-99]: <input type='text' name='kor' value=${kor} style='width:3em;' onChange='this.form.submit();'>\n`);
-        res.write(`4. feladat: Te ${kor} éves vagy!\n`);
+        res.write(`Kérem a korod: <input type='text' name='kor' value=${korod} style='width:3em;' onChange='this.form.submit();'>\n`);
+        res.write(`Te ${korod} éves vagy!\n`);
 
         // <---- Fejezd be a kódolást
 
