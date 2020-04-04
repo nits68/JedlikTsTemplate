abe8e04016c9a9a5f0798c9fb2c58b3e885806ed
László Nits <nitslaszlo@gmail.com>
2020-04-04 07:47:51 +0200

    up

--- src/Content.ts
+++ src/Content.ts
@@ -17,6 +17,16 @@ export default class Content {
         res.write("<head>");
         res.write("<style>input, pre {font-family:monospace; font-size:1em; font-weight:bold;}</style>");
         res.write("<meta name='viewport' content='width=device-width, initial-scale=1.0'>");
+
+        // Font Awesome:
+        res.write("<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.8.2/css/all.css'>");
+        // Google Fonts:
+        res.write("<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'>");
+        // Bootstrap core CSS:
+        res.write("<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css'>");
+        // Material Design Bootstrap:
+        res.write("<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.15.0/css/mdb.min.css'>");
+
         res.write("<title>Jedlik Ts Template</title>");
         res.write("</head>");
         res.write("<body><form><pre>");
@@ -24,7 +34,6 @@ export default class Content {
         const params = url.parse(req.url as string, true).query;
 
         // Kezd a kódolást innen -->
-
         res.write("Egyszerű Hello World!\n");
 
         // Tetszőleges html teg-ek és attribútumok beépítése:
@@ -36,12 +45,25 @@ export default class Content {
         // akkor a "korod" változóba NaN érték kerül, ilyenkor legyen 18 év az értéke:
         if (isNaN(korod)) korod = 18;
 
-        res.write(`Kérem a korod: <input type='number' name='kor' value=${korod} style='width:3em;' onChange='this.form.submit();'>\n`);
+        res.write(`Kérem a korod: <input type='number' name='kor' value=${korod} style='max-width:100px;' onChange='this.form.submit();'>\n`);
         res.write(`Te ${korod} éves vagy!\n`);
+
+        res.write("Material Design for Bootstrap input demo:");
+        res.write("<div class='md-form'><i class='fas fa-envelope prefix'></i><input type='text' id='inputIconEx1' style='max-width:300px;' class='form-control'><label for='inputIconEx1'>E-mail cím</label></div>");
 
         // <---- Fejezd be a kódolást
 
-        res.write("</pre></form></body></html>");
+        res.write("</pre></form>");
+        // JQuery:
+        res.write("<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>");
+        // Bootstrap tooltips:
+        res.write("<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js'></script>");
+        // Bootstrap core JavaScript:
+        res.write("<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js'></script>");
+        // MDB core JavaScript:
+        res.write("<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.15.0/js/mdb.min.js'></script>");
+
+        res.write("</body></html>");
         res.end();
     }
 }
