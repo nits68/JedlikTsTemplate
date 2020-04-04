af19011f8a1e2159a10c2308195327a56021b4b5
László Nits <nitslaszlo@gmail.com>
2020-04-04 07:49:16 +0200

    up

--- src/Content.ts
+++ src/Content.ts
@@ -55,6 +55,7 @@ export default class Content {
         // <---- Fejezd be a kódolást
 
         res.write("</pre></form>");
+
         // JQuery:
         res.write("<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>");
         // Bootstrap tooltips:
