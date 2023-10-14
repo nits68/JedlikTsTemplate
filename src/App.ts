be31a89107207f71c4e40dca918dff8f58a6ba7b
nitslaszlo <nitslaszlo@gmail.com>
2023-10-14 04:51:15 +0200

    uograde

--- src/App.ts
+++ src/App.ts
@@ -1,11 +1,9 @@
 ﻿import http from "http";
 import content from "./Content";
-// import content2 from "./Content2";
 
 class Program {
     constructor() {
         http.createServer(content).listen(process.env.PORT || 8080);
-        // http.createServer(Content2.content).listen(process.env.PORT || 8080);
     }
 }
 
